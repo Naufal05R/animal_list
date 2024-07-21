9334ce483f38e9d65254ed3d14dd19f2b16441fc
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-21 18:55:16 +0700

    create PaginationArrow Component

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-const Pagination = () => {
-	return (
-		<div>Pagination</div>
-	)
-}
+const PaginationArrow = ({
+  href,
+  direction,
+  isDisabled,
+}: {
+  href: string;
+  direction: "left" | "right";
+  isDisabled?: boolean;
+}) => {
+  const className = "flex h-10 w-10 items-center justify-center text-sm border";
+  const icon =
+    direction === "left" ? (
+      <MdChevronLeft size={20} />
+    ) : (
+      <MdChevronRight size={20} />
+    );
 
-export default Pagination
+  return isDisabled ? (
+    <div className={className}>{icon}</div>
+  ) : (
+    <Link href={href} className={className}>
+      {icon}
+    </Link>
+  );
+};
+
+const Pagination = ({ totalPage }: { totalPage?: number }) => {
+  return <article className="">
+		
+	</article>;
+};
+
+export default Pagination;
